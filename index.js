855f3f8ebedf6f901c06355c0a5cf10109b52314
jeelnarola <narolajeel29@gmail.com>
2024-11-27 22:07:24 +0530

    ETM2

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
-// const DataBase = require('./configs/db')
+const DataBase = require('./configs/db')
 const app = express()
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
-
-
-mongoose
-.connect(process.env.MONGODB_URL)
-.then(() => console.log("MongoDB connected"))
-.catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-});
-
 app.get('/',(req,res)=>{
     res.status({msg:"Welcome To Expense Tracker Application."})
 })
 
-// DataBase()
-// app.listen(process.env.PORT,()=>{
-//     console.log("Server Start :- ",process.env.PORT);
-    
-// })
+app.listen(process.env.PORT,()=>{
+    console.log("Server Start :- ",process.env.PORT);
+    DataBase()
+})
 
 module.exports = app
