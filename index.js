e019d46361c7ac34c0c2ebde8e6266da862ac20e
jeelnarola <narolajeel29@gmail.com>
2024-11-27 21:49:58 +0530

    ETM1

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
+
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+});
+
 app.get('/',(req,res)=>{
     res.status({msg:"Welcome To Expense Tracker Application."})
 })
-
-
-    mongoose.connect(process.env.MONGODB_URL)
-    console.log(process.env.MONGODB_URL)
-    console.log("DataBase Connect");
 
 // DataBase()
 // app.listen(process.env.PORT,()=>{
